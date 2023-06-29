fa2326adc6990851572b89219cd705dd501ecbf1
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-06-29 17:35:48 +0530

    feat: operators
    Section 13 - Understanding Operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
+
   private firstObsSubscription: Subscription;
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
     // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // });
-    const customIntervalObservable = Observable.create((observer) => {
+    const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
@@ -30,21 +33,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        console.log('Completed!');
-      }
-    );
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
+    });
   }
 
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
   }
+
 }
